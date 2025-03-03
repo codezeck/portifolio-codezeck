46910415e81429990deb124a027697b6e899b176
Ezequiel Silva <ezequieldasilvamanco18@gmail.com>
2025-03-03 16:45:58 -0300

    update

--- src/javascript/navigation.js
+++ src/javascript/navigation.js
@@ -1,62 +1,42 @@
 /* função botão mobile */
-
 function btnMenu() {
-    const nav = document.querySelector('.tab-buttons');
-    if (nav.classList.contains('hidden')) {
-        nav.classList.remove('hidden');
-    } else if (!nav.classList.contains('hidden')){
-        nav.classList.add('hidden');
-    }
+  const nav = document.querySelector('.tab-buttons');
+  nav.classList.toggle('hidden');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    const button = document.querySelector('.tab-buttons');
-
-    button.addEventListener('click', (event) => {
-        if (event.target === button || button.contains(event.target)) {
-            button.classList.toggle('hidden');
-        }
-    });
+  const button = document.querySelector('.tab-buttons');
+  button.addEventListener('click', (event) => {
+      if (event.target === button || button.contains(event.target)) {
+          btnMenu();
+      }
+  });
 });
-
 
 /* função navegação */
 
 function setActive(button) {
-    // Remove a classe 'active' de todos os botões
-    const buttons = document.querySelectorAll('.tab-btn');
-    buttons.forEach(btn => {
-      btn.classList.remove('active');
-    });
 
-    // Adiciona a classe 'active' ao botão clicado
-    button.classList.add('active');
+  document.querySelectorAll('.tab-btn').forEach(btn => btn.classList.remove('active'));
 
-    // Esconde todos os conteúdos
-    const contents = document.querySelectorAll('.hidden-content');
-    contents.forEach(content => {
-      content.style.display = 'none';
-    });
+  button.classList.add('active');
 
-    // Mostra o conteúdo correspondente ao botão clicado
-    const contentId = button.getAttribute('content-id');
-    const contentDiv = document.getElementById(contentId);
-    if (contentDiv) {
+  document.querySelectorAll('.hidden-content').forEach(content => content.style.display = 'none');
+
+  const contentId = button.getAttribute('content-id');
+  const contentDiv = document.getElementById(contentId);
+  if (contentDiv) {
       contentDiv.style.display = 'flex';
-    }
   }
+}
 
-  // Adiciona o evento de clique a todos os botões com a classe 'tab-btn'
-  document.querySelectorAll('.tab-btn').forEach(button => {
-    button.addEventListener('click', function() {
+document.querySelectorAll('.tab-btn').forEach(button => {
+  button.addEventListener('click', function() {
       setActive(this);
-    });
   });
+});
 
-  // Mostra o conteúdo 'Home' por padrão
-  document.getElementById('home').style.display = 'flex';
-
-
+document.getElementById('home').style.display = 'flex';
 
 /* animações */
 
